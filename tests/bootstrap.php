08450cfa5472f4a4b64214babb5392302cba5ea0
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2023-06-11 16:50:00 +0200

    simplify test bootstrapping

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,12 +11,6 @@ declare(strict_types=1);
  * installed as a dependency of an application.
  */
 
-use Cake\Cache\Cache;
-use Cake\Core\Configure;
-use Cake\Datasource\ConnectionManager;
-use Cake\TestSuite\Fixture\SchemaLoader;
-use function Cake\Core\env;
-
 $findRoot = function ($root) {
     do {
         $lastRoot = $root;
@@ -30,40 +24,4 @@ $findRoot = function ($root) {
 $root = $findRoot(__FILE__);
 unset($findRoot);
 chdir($root);
-
-require_once 'vendor/autoload.php';
-
-define('ROOT', $root . DS . 'tests' . DS . 'test_app' . DS);
-define('APP', ROOT . 'TestApp' . DS);
-define('CONFIG', APP);
-define('TMP', sys_get_temp_dir() . DS);
-define('CACHE', TMP . 'cache' . DS);
-
-Configure::write('App', [
-    'debug' => true,
-    'namespace' => 'TestApp',
-    'paths' => [
-        'plugins' => [ROOT . 'Plugin' . DS],
-        'templates' => [ROOT . 'templates' . DS],
-    ],
-    'encoding' => 'UTF-8',
-]);
-
-Cache::setConfig([
-    '_cake_core_' => [
-        'engine' => 'File',
-        'prefix' => 'cake_core_',
-        'serialize' => true,
-        'path' => CACHE,
-    ],
-]);
-
-if (!getenv('DB_URL')) {
-    putenv('DB_URL=sqlite:///:memory:');
-}
-ConnectionManager::setConfig('test', ['url' => getenv('DB_URL')]);
-
-if (env('FIXTURE_SCHEMA_METADATA')) {
-    $loader = new SchemaLoader();
-    $loader->loadInternalFile(env('FIXTURE_SCHEMA_METADATA'));
-}
+require $root . '/vendor/cakephp/cakephp/tests/bootstrap.php';
